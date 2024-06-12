d59c537960ee0df49f019c2d6376b3f8ddcc0e9a
S00201014-ATU <S00201014@atu.ie>
2024-06-12 11:00:10 +0100

    Amend service file

--- frontend/frontend/src/app/services/product.service.ts
+++ frontend/frontend/src/app/services/product.service.ts
@@ -4,24 +4,23 @@ import { Product } from "../models/product";
 @Injectable({
   providedIn: 'root'
 })
-
-export class ProductService{
-  private products:Product[] = [
-    {id: 1, name: 'Portugal', price: 50.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/18/nkshdzfrkiXGUmC.jpg', description: 'More glory for Ronaldo?'},
-    {id: 2, name: 'England', price: 20.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/eIBe01GsLB2TAEG.jpg', description: 'Is it coming home?'},
-    {id: 1, name: 'France', price: 100.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/19/IHofQUS745D2Zl4.jpg', description: 'Mbappe to lead the way?'},
-    {id: 1, name: 'Netherlands', price: 10.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/Io9t2jHZKVdvr0W.jpg', description: 'Could they be the dark horses?'},
-    {id: 1, name: 'Ireland', price: 80.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/22/jmWxmFIOuo66e16.jpg', description: 'Why are we not there?'},
-    {id: 1, name: 'Romania', price: 5.00, imageUrl: 'https://cdn.footballkitarchive.com/2023/03/21/Lbhju3KFyexzIsW.jpg', description: 'How many goals will they concede?'},
+export class ProductService {
+  private products: Product[] = [
+    { id: 1, name: 'Portugal', price: 50.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/18/nkshdzfrkiXGUmC.jpg', description: 'More glory for Ronaldo?' },
+    { id: 2, name: 'England', price: 20.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/eIBe01GsLB2TAEG.jpg', description: 'Is it coming home?' },
+    { id: 3, name: 'France', price: 100.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/19/IHofQUS745D2Zl4.jpg', description: 'Mbappe to lead the way?' },
+    { id: 4, name: 'Netherlands', price: 10.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/Io9t2jHZKVdvr0W.jpg', description: 'Could they be the dark horses?' },
+    { id: 5, name: 'Ireland', price: 80.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/22/jmWxmFIOuo66e16.jpg', description: 'Why are we not there?' },
+    { id: 6, name: 'Romania', price: 5.00, imageUrl: 'https://cdn.footballkitarchive.com/2023/03/21/Lbhju3KFyexzIsW.jpg', description: 'How many goals will they concede?' },
   ];
 
-  constructor() {}
+  constructor() { }
 
   getProducts(): Product[] {
     return this.products;
   }
 
-  getProductById(id: number): Product | undefined{
+  getProductById(id: number): Product | undefined {
     return this.products.find(product => product.id === id);
   }
 
@@ -39,5 +38,4 @@ export class ProductService{
   deleteProduct(id: number): void {
     this.products = this.products.filter(product => product.id !== id);
   }
-
 }
