8c17f09c4d4431bc779fbe7578f0e0b073010ed9
S00201014-ATU <S00201014@atu.ie>
2024-06-12 11:13:40 +0100

    Allow user to view product details through clicking on the image

--- frontend/frontend/src/app/product-list/product-list.component.ts
+++ frontend/frontend/src/app/product-list/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
@@ -10,12 +11,13 @@ import { Product } from '../models/product';
 export class ProductListComponent implements OnInit {
   products: Product[] = [];
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private router:Router) { }
 
   ngOnInit(): void {
     this.products = this.productService.getProducts();
   }
 
   viewProductDetails(productId: number): void {
+    this.router.navigate(['/product', productId]);
   }
 }
