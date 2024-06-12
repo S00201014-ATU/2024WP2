ea7ca65cde5cee080706104ed6806121e0e92ef7
S00201014-ATU <S00201014@atu.ie>
2024-06-12 11:31:04 +0100

    Start work on product details page

--- frontend/frontend/src/app/product-details/product-details.component.ts
+++ frontend/frontend/src/app/product-details/product-details.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from './../services/product.service';
+import { Product } from './../models/product';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
-  styleUrl: './product-details.component.css'
+  styleUrls: ['./product-details.component.css']
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  product: Product | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id !== null) {
+      const productId = +id;
+      const foundProduct = this.productService.getProductById(productId);
+      if (foundProduct !== undefined) {
+        this.product = foundProduct;
+      } else {
+        console.error(`Product with ID ${productId} not found.`);
+      }
+    } else {
+      console.error("Product ID is missing from route parameters.");
+    }
+  }
 }
