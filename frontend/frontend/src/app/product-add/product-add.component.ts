039fbf737e919af16d7e4bf16128c318dde6b499
S00201014-ATU <S00201014@atu.ie>
2024-06-12 12:27:55 +0100

    create product add page

--- frontend/frontend/src/app/product-add/product-add.component.ts
+++ frontend/frontend/src/app/product-add/product-add.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { Product } from '../models/product';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
   styleUrl: './product-add.component.css'
 })
-export class ProductAddComponent {
+export class ProductAddComponent implements OnInit{
+  newProduct: Product = {id: 0, name: '', price:0, imageUrl: '', description: ''};
 
+  constructor(private productService:ProductService, private router:Router){ }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubmit(): void {
+    this.productService.addProduct(this.newProduct);
+    this.router.navigate(['/']);
+  }
 }
