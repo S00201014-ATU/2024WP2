4cab2bc373a7265d353e624153fb2ce7e7ac71a6
S00201014-ATU <S00201014@atu.ie>
2024-06-12 15:35:20 +0100

    create connection to cluster

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { MongoClient } = require('mongodb')
 
 const app = express;
 const PORT = process.env.PORT || 3000;
+
+const uri = "mongodb+srv://Matthew902:Manu1234@wp2repeat2024.bmdxgib.mongodb.net/?retryWrites=true&w=majority&appName=WP2Repeat2024"
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true});
+
+async function connect() {
+    try{
+        await client.connect();
+        console.log('Connected to MongoDB');
+    } catch (error) {
+        console.error('Error connecting to MongoDB', error);
+    }
+}
+
+connect();
 
 app.use(bodyParser.json());
 app.use(cors());
