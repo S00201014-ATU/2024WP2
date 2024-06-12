0ca8a933f3787a120cd1b0cea1712fa3a1ab0047
S00201014-ATU <S00201014@atu.ie>
2024-06-12 12:45:56 +0100

    fix image displaying issue

--- frontend/frontend/src/app/services/product.service.ts
+++ frontend/frontend/src/app/services/product.service.ts
@@ -6,12 +6,12 @@ import { Product } from "../models/product";
 })
 export class ProductService {
   private products: Product[] = [
-    { id: 1, name: 'Portugal', price: 50.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/18/nkshdzfrkiXGUmC.jpg', description: 'More glory for Ronaldo?' },
-    { id: 2, name: 'England', price: 20.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/eIBe01GsLB2TAEG.jpg', description: 'Is it coming home?' },
-    { id: 3, name: 'France', price: 100.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/19/IHofQUS745D2Zl4.jpg', description: 'Mbappe to lead the way?' },
-    { id: 4, name: 'Netherlands', price: 10.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/Io9t2jHZKVdvr0W.jpg', description: 'Could they be the dark horses?' },
-    { id: 5, name: 'Ireland', price: 80.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/22/jmWxmFIOuo66e16.jpg', description: 'Why are we not there?' },
-    { id: 6, name: 'Romania', price: 5.00, imageUrl: 'https://cdn.footballkitarchive.com/2023/03/21/Lbhju3KFyexzIsW.jpg', description: 'How many goals will they concede?' },
+    { id: 1, name: 'Portugal', price: 50.00, imageUrl: 'https://www.sportsdirect.com/images/products/37096608_l.jpg', description: 'More glory for Ronaldo?' },
+    { id: 2, name: 'England', price: 20.00, imageUrl: 'https://www.sportsdirect.com/images/products/37078101_l.jpg', description: 'Is it coming home?' },
+    { id: 3, name: 'France', price: 100.00, imageUrl: 'https://www.sportsdirect.com/images/products/37997518_l.jpg', description: 'Mbappe to lead the way?' },
+    { id: 4, name: 'Netherlands', price: 10.00, imageUrl: 'https://www.sportsdirect.com/images/products/37263512_l.jpg', description: 'Could they be the dark horses?' },
+    { id: 5, name: 'Ireland', price: 80.00, imageUrl: 'https://www.soccerbox.com/media/catalog/product/cache/f4b6407e5847ea579fdc5730945961f6/i/r/ireland-kids-home-shirt-23.jpg', description: 'Why are we not there?' },
+    { id: 6, name: 'Scotland', price: 25.00, imageUrl: 'https://www.sportsdirect.com/images/products/36099218_l.jpg', description: 'How many goals will they concede?' },
   ];
 
   constructor() { }
