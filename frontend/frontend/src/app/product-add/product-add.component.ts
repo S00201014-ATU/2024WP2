552809b518f5ddeadb8999769582e8eb42d20e7f
S00201014-ATU <S00201014@atu.ie>
2024-06-12 14:43:09 +0100

    Make validation for add form

--- frontend/frontend/src/app/product-add/product-add.component.ts
+++ frontend/frontend/src/app/product-add/product-add.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product';
 import { Router } from '@angular/router';
@@ -6,19 +6,26 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-product-add',
   templateUrl: './product-add.component.html',
-  styleUrl: './product-add.component.css'
+  styleUrls: ['./product-add.component.css']
 })
-export class ProductAddComponent implements OnInit{
-  newProduct: Product = {id: 0, name: '', price:0, imageUrl: '', description: ''};
+export class ProductAddComponent {
+  newProduct: Product = {
+    id: 0,
+    name: '',
+    price: 0,
+    imageUrl: '',
+    description: ''
+  };
+  errorMessage: string = '';
 
-  constructor(private productService:ProductService, private router:Router){ }
-
-  ngOnInit(): void {
-
-  }
+  constructor(private productService: ProductService, private router: Router) {}
 
   onSubmit(): void {
-    this.productService.addProduct(this.newProduct);
-    this.router.navigate(['/']);
+    if (this.newProduct.name && this.newProduct.price && this.newProduct.imageUrl && this.newProduct.description) {
+      this.productService.addProduct(this.newProduct);
+      this.router.navigate(['/']);
+    } else {
+      this.errorMessage = "Please fill in all fields before adding the product.";
+    }
   }
 }
