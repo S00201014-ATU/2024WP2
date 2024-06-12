0f4c473390ef7ee018287a01203b5639a9db8b28
S00201014-ATU <S00201014@atu.ie>
2024-06-12 10:23:34 +0100

    Create service file and add initial products

---
+++ frontend/frontend/src/app/services/product.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from "@angular/core";
+import { Product } from "../models/product";
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class ProductService{
+  private products:Product[] = [
+    {id: 1, name: 'Portugal', price: 50.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/18/nkshdzfrkiXGUmC.jpg', description: 'More glory for Ronaldo?'},
+    {id: 2, name: 'England', price: 20.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/eIBe01GsLB2TAEG.jpg', description: 'Is it coming home?'},
+    {id: 1, name: 'France', price: 100.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/19/IHofQUS745D2Zl4.jpg', description: 'Mbappe to lead the way?'},
+    {id: 1, name: 'Netherlands', price: 10.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/20/Io9t2jHZKVdvr0W.jpg', description: 'Could they be the dark horses?'},
+    {id: 1, name: 'Ireland', price: 80.00, imageUrl: 'https://cdn.footballkitarchive.com/2024/03/22/jmWxmFIOuo66e16.jpg', description: 'Why are we not there?'},
+    {id: 1, name: 'Romania', price: 5.00, imageUrl: 'https://cdn.footballkitarchive.com/2023/03/21/Lbhju3KFyexzIsW.jpg', description: 'How many goals will they concede?'},
+  ];
+
+  constructor() {}
+
+  getProducts(): Product[] {
+    return this.products;
+  }
+
+  getProductById(id: number): Product | undefined{
+    return this.products.find(product => product.id === id);
+  }
+
+  addProduct(product: Product): void {
+    this.products.push(product);
+  }
+
+  updateProduct(updatedProduct: Product): void {
+    const index = this.products.findIndex(product => product.id === updatedProduct.id);
+    if (index !== -1) {
+      this.products[index] = updatedProduct;
+    }
+  }
+
+  deleteProduct(id: number): void {
+    this.products = this.products.filter(product => product.id !== id);
+  }
+
+}
