6929644f57b3d71bd64f871a65bf61f38917292c
S00201014-ATU <S00201014@atu.ie>
2024-06-20 11:42:17 +0100

    update app routing module

--- frontend/frontend/src/app/app-routing.module.ts
+++ frontend/frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
-import { ProductEditComponent } from './product-edit/product-edit.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
@@ -13,10 +16,16 @@ const routes: Routes = [
     path: 'products/:id', component:ProductDetailsComponent
   },
   {
-    path: 'edit-product/:id', component:ProductEditComponent
+    path: 'edit-product/:id', component:ProductEditComponent, canActivate: [AuthGuard]
   },
   {
-    path: 'add-product', component: ProductAddComponent
+    path: 'add-product', component: ProductAddComponent, canActivate: [AuthGuard]
+  },
+  {
+    path: 'login', component:LoginComponent
+  },
+  {
+    path: 'register', component:RegisterComponent
   }
 ];
 
