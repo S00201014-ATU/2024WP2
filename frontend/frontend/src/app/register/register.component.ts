49f62e5f9e7b23847e017f3979443b6741830b3c
S00201014-ATU <S00201014@atu.ie>
2024-06-20 11:21:27 +0100

    code register component

--- frontend/frontend/src/app/register/register.component.ts
+++ frontend/frontend/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService:AuthService, private router:Router) {}
+
+  register(){
+    this.authService.register(this.username, this.password).subscribe(
+      res => {
+        alert('Registration successful');
+        this.router.navigate(['/login']);
+      },
+      err => {
+        console.error('Error registering', err);
+        alert('Registration failed');
+      }
+    );
+  }
 }
