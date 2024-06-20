bc080d6b1984c0839fca01108f8e0006e11f0950
S00201014-ATU <S00201014@atu.ie>
2024-06-20 10:41:44 +0100

    code login page

--- frontend/frontend/src/app/login/login.component.ts
+++ frontend/frontend/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  username: string = '';
+  password: string = '';
+
+  constructor(private authService: AuthService, private router:Router) {}
+
+  login() {
+    this.authService.login(this.username, this.password).subscribe(
+      res => {
+        localStorage.setItem('token', res.token);
+        this.router.navigate(['/']);
+      },
+      err => {
+        console.error('Error logging in', err);
+        alert('Login failed');
+      }
+    )
+  }
 }
