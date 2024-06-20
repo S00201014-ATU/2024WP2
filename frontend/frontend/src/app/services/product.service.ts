d5db24d024f9c106a2f499d247510fe6c4285333
S00201014-ATU <S00201014@atu.ie>
2024-06-20 10:16:10 +0100

    update product service to include the token

--- frontend/frontend/src/app/services/product.service.ts
+++ frontend/frontend/src/app/services/product.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +10,30 @@ import { Product } from '../models/product';
 export class ProductService {
   private apiUrl = 'http://localhost:3000/products';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorisation', `Bearer ${token}`);
+  }
 
   getProducts(): Observable<Product[]> {
-    return this.http.get<Product[]>(this.apiUrl);
+    return this.http.get<Product[]>(this.apiUrl, {headers: this.getAuthHeaders()});
   }
 
   getProductById(id: string): Observable<Product> {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+    return this.http.get<Product>(`${this.apiUrl}/${id}`, {headers: this.getAuthHeaders()});
   }
 
   addProduct(product: Product): Observable<Product> {
-    return this.http.post<Product>(this.apiUrl, product);
+    return this.http.post<Product>(this.apiUrl, product, {headers: this.getAuthHeaders()});
   }
 
   updateProduct(id: string, updatedProduct: Partial<Product>): Observable<any> {
-    return this.http.put(`${this.apiUrl}/${id}`, updatedProduct);
+    return this.http.put(`${this.apiUrl}/${id}`, updatedProduct, {headers: this.getAuthHeaders()});
   }
 
   deleteProduct(id: string): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`, {headers: this.getAuthHeaders()});
   }
 }
