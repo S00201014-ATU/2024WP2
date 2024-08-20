79aa3061ef5519e394c287eae4f9fbed6ed40db7
S00201014-ATU <S00201014@atu.ie>
2024-08-20 11:58:51 +0100

    Commenting all files - login component

--- frontend/frontend/src/app/login/login.component.ts
+++ frontend/frontend/src/app/login/login.component.ts
@@ -12,15 +12,18 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
 
-  constructor(private authService: AuthService, private router:Router) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
+  // Method to handle user login
   login() {
     this.authService.login(this.username, this.password).subscribe(
       res => {
+        // Save the token to local storage and navigate to the homepage
         localStorage.setItem('token', res.token);
         this.router.navigate(['/']);
       },
       err => {
+        // Log error and show a message if login fails
         console.error('Error logging in', err);
         alert('Login failed');
       }
